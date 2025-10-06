209cbb5024b114176563d011109d9fa880544b0e
MohitSingh0707 <mohitsingh22410@gmail.com>
2025-10-06 10:47:34 +0530

    Exploring get ,post,delete

--- src/app.js
+++ src/app.js
@@ -2,8 +2,16 @@ const express = require('express');
 
 const app = express();
 
-app.get("/",(req,res) => {
-    res.send("Hello World");
+app.get("/user",(req,res) => {
+    res.send({firstName: "Mohit",lastname: "Singh"});
+});
+
+app.post("/user",(req,res) => {
+    res.send("Data posted sucessfully"); 
+});
+
+app.delete("/user",(req,res) => {
+    res.send("Data deleted sucessfully"); 
 });
 
 app.get("/hello",(req,res) => {
