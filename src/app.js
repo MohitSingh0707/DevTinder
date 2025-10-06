9022087a42223eb9798f99c12a673884d8406573
MohitSingh0707 <mohitsingh22410@gmail.com>
2025-10-06 11:02:30 +0530

    Playing with dynamic routes

--- src/app.js
+++ src/app.js
@@ -2,20 +2,9 @@ const express = require('express');
 
 const app = express();
 
-app.get("/user",(req,res) => {
+app.get("/user/:userId/:name/:password",(req,res) => {
+    console.log(req.params)
     res.send({firstName: "Mohit",lastname: "Singh"});
-});
-
-app.post("/user",(req,res) => {
-    res.send("Data posted sucessfully"); 
-});
-
-app.delete("/user",(req,res) => {
-    res.send("Data deleted sucessfully"); 
-});
-
-app.get("/hello",(req,res) => {
-    res.send("Hello Hello!");
 });
 
 app.get("/test",(req,res) => {
