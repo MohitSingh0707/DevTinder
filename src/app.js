af7294c858d56200f1c55dd7bd3e202b8135792f
MohitSingh0707 <mohitsingh22410@gmail.com>
2025-10-17 12:08:00 +0530

    Getting all user from databse

--- src/app.js
+++ src/app.js
@@ -8,20 +8,21 @@ app.use(express.json());
 
 // API endpoint for user registration
 
-app.post("/signup",async (req,res) => {
+// Signup Route
+app.post("/signup", async (req, res) => {
   try {
     // step-1 ==> Get the input fields from the request body
-    const {firstName,lastName,emailId,password,age,gender} = req.body;
+    const { firstName, lastName, emailId, password, age, gender } = req.body;
 
     // step-2 ==> Validate the input fields
-    if(!firstName || !emailId || !password || !age || !gender){
-      return res.status(400).json({message : "All fields are required"});
+    if (!firstName || !emailId || !password || !age || !gender) {
+      return res.status(400).json({ message: "All fields are required" });
     }
 
     // step-3 ==> Check if the user already exists
-    const existingUser = await User.findOne({emailId});
-    if(existingUser){
-      return res.status(400).json({message : " User Already Exists"});
+    const existingUser = await User.findOne({ emailId });
+    if (existingUser) {
+      return res.status(400).json({ message: " User Already Exists" });
     }
 
     // step-4 ==> Create a new user
@@ -31,17 +32,28 @@ app.post("/signup",async (req,res) => {
       emailId,
       password,
       age,
-    })
+    });
 
     // step-5 ==> Save the user to the database
     await user.save();
     // step-6 ==> Return a success response
-    return res.status(201).json({message : "User registered successfully"});
+    return res.status(201).json({ message: "User registered successfully" });
   } catch (error) {
-    console.error("Error during user registration",error);
-    return res.status(500).json({message : "Internal Server Error"});
+    console.error("Error during user registration", error);
+    return res.status(500).json({ message: "Internal Server Error" });
   }
-})
+});
+
+// Feed up Route - GET/feed - get all usrs from the database
+app.get("/feed", async (req, res) => {
+  try {
+    const users = await User.find({});
+    return res.status(200).json(users);
+  } catch (error) {
+    console.error("Error fetching users", error);
+    return res.status(500).json({ message: "Internal Server Error" });
+  }
+});
 
 connectDB()
   .then(() => {
